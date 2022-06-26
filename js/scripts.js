0bf7f7e992bb32ec8ca0add21fbf75f407ed9ccf
cjhart34 <104225229+cjhart34@users.noreply.github.com>
2022-06-25 20:42:24 -0500

    added Pokemon names to JavaScript

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+    {name: 'Geodude', height: 0.4, type: [Rock, Ground]}
+    {name: 'Graveler', height: 1, type: [Rock, Ground]}
+    {name: 'Golem', height 1.4, type: [Rock, Ground]}
+]
