9e4d8e02919ba83453da90b36ffbc7517965d2c6
cjhart34 <104225229+cjhart34@users.noreply.github.com>
2022-06-27 19:28:03 -0500

    added conditional for large pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 let pokemonList = [
-    {name: 'Geodude', height: 0.4, type: ["Rock", "Ground"]}
-    {name: 'Graveler', height: 1, type: ["Rock", "Ground"]}
-    {name: 'Golem', height 1.4, type: ["Rock", "Ground"]}
-]
+    {name: 'Geodude', height: 0.4, type: ["Rock", "Ground"]},
+    {name: 'Graveler', height: 1, type: ["Rock", "Ground"]},
+    {name: 'Golem', height: 1.4, type: ["Rock", "Ground"]}
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1) { // Highlighting big Pokemon 
+      document.write('<p class="special">' + pokemonList[i].name, '(height: ' + pokemonList[i].height + ')-Wow! That\'s a big Pokemon!</p>')
+    } else {
+      document.write('<p>' + pokemonList[i].name, '(height: ' + pokemonList[i].height + ')</p>')
+    }
+  }
