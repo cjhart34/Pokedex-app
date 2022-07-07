7ab7819bae8749f738fefffa8eaf323e8ba5cafb
cjhart34 <104225229+cjhart34@users.noreply.github.com>
2022-07-07 07:40:27 -0500

    added forEach and IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,45 @@
-let pokemonList = [
-    {name: 'Geodude', height: 0.4, type: ["Rock", "Ground"]},
-    {name: 'Graveler', height: 1, type: ["Rock", "Ground"]},
-    {name: 'Golem', height: 1.4, type: ["Rock", "Ground"]}
-];
+ let pokemonRepository = (function() {
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 1) { // Highlighting big Pokemon 
-      document.write('<p class="special">' + pokemonList[i].name, '(height: ' + pokemonList[i].height + ')-Wow! That\'s a big Pokemon!</p>')
-    } else {
-      document.write('<p>' + pokemonList[i].name, '(height: ' + pokemonList[i].height + ')</p>')
+ 
+    let pokemonList = [
+            {name: 'Geodude', 
+            height: 0.4, 
+            type: ["Rock", "Ground"]},
+
+            {name: 'Graveler',
+             height: 1, 
+             type: ["Rock", "Ground"]},
+
+            {name: 'Golem', 
+            height: 1.4, 
+            type: ["Rock", "Ground"]}
+        ];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-  }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+let pokemonList = pokemonRepository.getAll();
+
+pokemonList.forEach(function (pokemon) {
+    if (pokemon.height >= 1) {
+        document.write(`Name: ${pokemon.name} Height: ${pokemon.height} -- Wow! That\'s a big Pokemon!<br>`)
+    } else if (pokemon.height > 1 && pokemon.height < 0.5) {
+        document.write(`Name: ${pokemon.name} Height: ${pokemon.height} -- That\'s an average Pokemon. <br>`)
+    }
+    else {
+        document.write(`Name: ${pokemon.name} Height: ${pokemon.height} --That\'s a small Pokemon.<br>`)
+    }
+})
+
+
