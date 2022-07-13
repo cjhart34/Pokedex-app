484d3ef0756a196dd1d3f7f21cb61abcabf62c26
cjhart34 <104225229+cjhart34@users.noreply.github.com>
2022-07-12 22:40:18 -0500

    Added modal to the app

--- js/scripts.js
+++ js/scripts.js
@@ -16,10 +16,7 @@
         // }
     
     }
-  // retrieves pokemon information to be output in console
-    function showDetails(pokemon){
-        console.log(pokemon);
-    }
+    
 // returns list of pokemon and characteristics
     function getAll() {
         return pokemonList;
@@ -42,14 +39,13 @@
         return fetch(apiUrl).then(function (response) {
             return response.json();
         }).then(function (json) {
-            json.results.forEach(function (item)
-            {
+            json.results.forEach(function (item) {
                 let pokemon = {
                     name: item.name,
                     detailsUrl: item.url
                 };
                 add(pokemon);
-                console.log(pokemon);
+                // console.log(pokemon);
             });
         }).catch(function (e) {
             console.error(e);
@@ -68,19 +64,90 @@
         });
     }
     function showDetails(pokemon) { //Function for the event listener
-        pokemonRepository.loadDetails(pokemon).then(function () {
+        loadDetails(pokemon).then(function () {
             console.log(pokemon);
+            showModal(pokemon);
         });
     }
+    let modalContainer = document.querySelector('#modal-container');
+  
+
+    function showModal(pokemon) {
+        modalContainer.innerHTML = ' ';
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
+        modal.setAttribute('pointer-action', 'none');
+
+        let closeButtonElement = document.createElement('button');
+        closeButtonElement.classList.add('modal-close');
+        // closeButtonElement.innerText = 'Close';
+        closeButtonElement.addEventListener('click', hideModal);
+        
+        let titleElement = document.createElement('h1');
+        titleElement.innerText = pokemon.name;
+
+        let contentElement = document.createElement('p');
+        contentElement.innerHTML = "Height: "+pokemon.height+"<br>";
+
+        let imageElement = document.createElement('img');
+        imageElement.classList.add('pokemon-image');
+        imageElement.src = pokemon.imageUrl;
+
+        let types = [];
+        pokemon.types.forEach(function(typeObj){
+          types.push(" "+typeObj.type.name);
+        });
+        
+        if (types.length<2) {
+          contentElement.innerHTML += "Type:";
+        } else {
+          contentElement.innerHTML += "Types:";
+        }
+        contentElement.innerHTML += types.toString();
+   
+        modal.appendChild(imageElement);
+        modal.appendChild(closeButtonElement);
+        modal.appendChild(titleElement);
+        modal.appendChild(contentElement);
+        modalContainer.appendChild(modal);
+        modalContainer.classList.add('is-visible');
+       
+       
+    };
+
+    function hideModal() {
+        modalContainer.classList.remove('is-visible');
+    };
+
+    modalContainer.addEventListener('click', (e) => {
+        let target = e.target;
+        if(target === modalContainer) {
+            hideModal();
+        }
+    });
+
+    // document.querySelector('#show-modal').addEventListener('click', () => {
+    //     showModal('Modal title', 'This is the modal content');
+    // });
+
+    window.addEventListener('keydown', (e) => {
+        // let modalContainer = document.querySelector('modal-container');
+        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+            hideModal();
+        }
+    });
+
     return {
         add: add,
         getAll: getAll,
         addListItem: addListItem,
         loadList: loadList,
-        loadDetails: loadDetails,
-        showDetails: showDetails
+        loadDetails: loadDetails
     };
+
 })();
+
+
 
 console.log(pokemonRepository.getAll());
 
@@ -90,3 +157,5 @@ pokemonRepository.loadList().then(function() {
         pokemonRepository.addListItem(pokemon)
     });
 });
+
+
