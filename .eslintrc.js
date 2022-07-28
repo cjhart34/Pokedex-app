449a9edde14bba8241982436bfa6adb6c2c45823
cjhart34 <104225229+cjhart34@users.noreply.github.com>
2022-07-27 22:41:14 -0500

    made final changes to app

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs':true,
+        'es6': true,
+        'jquery': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'rules': {
+        'quotes': ['error', 'single']
+    }
+    }
